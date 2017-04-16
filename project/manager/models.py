c83403bc08888937a3d521057d114d0afb67bfcc
privod <bespalov@krsk.ustech.ru>
2017-04-16 20:59:51 +0700

    Add Atm class in model

--- project/manager/models.py
+++ project/manager/models.py
@@ -2,4 +2,15 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Atm(models.Model):
+    ref = models.IntegerField(primary_key=True)
+    serial = models.CharField(max_length=32)
+    number = models.CharField(max_length=32)
+    producer = models.CharField(max_length=32)
+    model = models.CharField(max_length=64)
+    city = models.CharField(max_length=64)
+    address = models.CharField(max_length=1024)
+
+    def __str__(self):
+        return '{}: {}'.format(self.producer, self.serial)
