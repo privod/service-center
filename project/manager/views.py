c3241c3ddc9de0ee601190673a177c19a0222426
privod <bespalov@krsk.ustech.ru>
2017-04-16 22:04:42 +0700

    Добавлено простое представление.

--- project/manager/views.py
+++ project/manager/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
+from django.template.context import RequestContext
 
-# Create your views here.
+from manager.models import Atm
+
+
+def index(request):
+    atm_list = Atm.objects.all()
+    context = {'atm_list': atm_list}
+    return render(request, 'manager/index.html', context)
