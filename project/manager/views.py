f248a992e386ce1d5543ded3669bda39d59686ce
privod <bespalov@krsk.ustech.ru>
2017-04-23 23:20:07 +0700

    Представление списка УС переведено на классы

--- project/manager/views.py
+++ project/manager/views.py
@@ -2,11 +2,35 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import loader
 from django.template.context import RequestContext
+from django.core.urlresolvers import reverse
+from django.views.generic.edit import CreateView
+from django.views.generic.list import ListView
 
 from manager.models import Atm
 
 
+# Устаревший метод
 def index(request):
     atm_list = Atm.objects.all()
     context = {'atm_list': atm_list}
-    return render(request, 'manager/index.html', context)
+    return render(request, 'manager/atm_list.html', context)
+
+
+class AtmListView(ListView):
+    model = Atm
+
+
+class CreateAtmView(CreateView):
+    model = Atm
+    fields = [
+        'ref',
+        'serial',
+        'number',
+        'producer',
+        'model',
+        'city',
+        'address',
+    ]
+
+    def get_success_url(self):
+        return reverse('atm-list')
